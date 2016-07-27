367fd4ca87e42876dad2cf133d863c197a694bdd
Troy <troy.cornwall@healthlink.net>
2016-07-27 14:27:26 +1200

    update to latest

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,5 +5,5 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'static/locales',
     'ROOT_PATH': None,
-    'GOOGLEMAPS_KEY': 'AIzaSyCNejn6kACgpHEkOEqNHldpxfNJL7i6qeI'
+    'GOOGLEMAPS_KEY': 'GMAPS KEY HERE'
 }
